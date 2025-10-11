4a27120a9af1cf8e0d162943c21f14fd647f944b
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-10-11 23:29:35 +0200

    Initial structure

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.pages = pages;
+  this.title = title;
+  this.author = author;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
