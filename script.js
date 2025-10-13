2f2a2c64d796095ee95e8987ea5b05a8b100aaa5
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-10-13 23:41:15 +0200

    Basic new book form added

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ function Book(title, author, pages) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
-  this.pages = pages;
   this.title = title;
   this.author = author;
+  this.pages = pages;
+
+  this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
-  
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+
+  console.log('Añadido:', book);
+  console.log(`${myLibrary}`);
 }
